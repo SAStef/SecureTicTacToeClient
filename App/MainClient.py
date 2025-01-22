134f64fdeb6b8627ae5905d6e80ea36a93592482
Stefan Artur Stefirta <128942076+SAStef@users.noreply.github.com>
2025-01-22 13:14:01 +0100

    reformat

--- App/MainClient.py
+++ App/MainClient.py
@@ -1,19 +1,19 @@
 import socket as sock
 
+
 def serverInit():
     s = sock.socket(sock.AF_INET, sock.SOCK_STREAM)
     s.connect(("34302.cyberteknologi.dk", 1063))
     print(s)
-    file = s.makefile('rb')
-    
-    
+    file = s.makefile("rb")
+
     firstLine = file.readline().strip()
-    
+
     print(firstLine)
-    
+
     while 1:
         nextLines = file.readline().strip()
         print(nextLines)
-        
-serverInit()
 
+
+serverInit()
