2ed29e4514e97a5bb07e9470138f7e4cd9e75113
Stefan Artur Stefirta <128942076+SAStef@users.noreply.github.com>
2025-01-22 10:40:28 +0100

    Create testServer.py

---
+++ testServer.py
@@ -0,0 +1,13 @@
+import socket as sock
+
+def serverInit():
+    s = sock.socket(sock.AF_INET, sock.SOCK_STREAM)
+    s.connect(("34302.cyberteknologi.dk", 1061))
+    
+    print(s)
+    
+        
+serverInit()
+
+
+
